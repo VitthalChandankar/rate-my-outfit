630d320d46ce77fd8587747fed0489fbf484df23
Darshanaj6 <tech1.electra24@gmail.com>
2025-10-08 21:10:37 +0530

    Successfully upgraded to sdK 54

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // other plugins you might have
+      'react-native-reanimated/plugin', // MUST BE LAST
+    ],
+  };
+  
